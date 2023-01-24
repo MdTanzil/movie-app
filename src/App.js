a958bba8456e3f0522f471ade39623c50230c267
Md Tanzil <mdtanzil.py@gmail.com>
2023-01-24 16:21:27 +0600

    Browser Router Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from "./components/About";
-
+import { Route, Routes } from "react-router";
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <About />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 }
