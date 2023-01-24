f04790dd561381ab1a54f6e742842d2cfec53b49
Md Tanzil <mdtanzil.py@gmail.com>
2023-01-24 16:35:48 +0600

    React Base Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid text-white">
-          <a className="navbar-brand" href="GoSomewhar">
+          <Link className="navbar-brand" to="/">
             Movie App
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,17 +23,17 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="GoSomewhar">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="GoSomewhar">
-                  Link
-                </a>
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
               </li>
               <li className="nav-item">
-                <a href="gosome" className="nav-link disabled">Disabled</a>
+                <Link to="gosome" className="nav-link disabled">Disabled</Link>
               </li>
             </ul>
             <form className="d-flex" role="search">
